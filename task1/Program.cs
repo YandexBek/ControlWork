4f791b366ff67b0d022172fab70f6c69e167b155
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-02-22 14:44:30 +0300

    add task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите элементы массива через пробел: ");
+string input = Console.ReadLine();
+
+string[] originalArray = input.Split(' ');
+string[] newArray = new string[originalArray.Length];
+int count = 0;
+
+foreach (string element in originalArray)
+{
+    if (element.Length <= 3)
+    {
+        newArray[count] = element;
+        count++;
+    }
+}
+
+Array.Resize(ref newArray, count);
+
+Console.WriteLine("Новый массив из строк, длина которых меньше или равна 3 символам:");
+
+foreach (string element in newArray)
+{
+    Console.Write(element + " ");
+}
